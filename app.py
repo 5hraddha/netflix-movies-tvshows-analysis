2e84b1fa125dce204852c0a0a4da38a1e6620d6a
Shraddha <mailmeatshraddha@gmail.com>
2023-05-28 19:36:32 -0400

    add scatter plots

--- app.py
+++ app.py
@@ -188,6 +188,43 @@ st.plotly_chart(
 
 # -------------------------------------------------------------------------------------------------
 # Set heading # 5
-st.header(
-    ":tv: Duration against the release year to check for any patterns in the length of content over time"
+st.header(":tv: Patterns in the duration of contents over time")
+
+# Divide the dataframe - netflix_data into two - netflix_movies and netflix_tv_shows
+netflix_movies = netflix_data[netflix_data["type"] == "Movie"]
+netflix_movies = netflix_movies.reset_index(drop=True)
+
+netflix_tv_shows = netflix_data[netflix_data["type"] == "TV Show"]
+netflix_tv_shows = netflix_tv_shows.reset_index(drop=True)
+
+# Clean up the duration column in both the dataframes - netflix_movies and netflix_tv_shows
+netflix_movies["duration"] = netflix_movies["duration"].str.replace(" min", "")
+netflix_movies["duration"] = netflix_movies["duration"].astype(int)
+
+netflix_tv_shows["duration"] = netflix_tv_shows["duration"].str.replace(" Season", "")
+netflix_tv_shows["duration"] = netflix_tv_shows["duration"].str.replace("s", "")
+netflix_tv_shows["duration"] = netflix_tv_shows["duration"].astype(int)
+
+# Plot a scatterplot for movies on Netflix - release_year on x-axis and duration on y-axis
+fig_scatter_movies = px.scatter(
+    netflix_movies,
+    x="release_year",
+    y="duration",
+    title="Distribution of duration of movies against the year of release",
+    height=800,
 )
+fig_scatter_movies.update_layout(yaxis_title="Duration (in mins)")
+fig_scatter_movies.update_layout(xaxis_title="Year of Release")
+st.plotly_chart(fig_scatter_movies, theme="streamlit", use_container_width=True)
+
+# Plot a scatterplot for movies on Netflix - release_year on x-axis and duration on y-axis
+fig_scatter_tv_shows = px.scatter(
+    netflix_tv_shows,
+    x="release_year",
+    y="duration",
+    title="Distribution of duration of TV shows against the year of release",
+    height=800,
+)
+fig_scatter_tv_shows.update_layout(yaxis_title="Duration (no of seasons)")
+fig_scatter_tv_shows.update_layout(xaxis_title="Year of Release")
+st.plotly_chart(fig_scatter_tv_shows, theme="streamlit", use_container_width=True)
