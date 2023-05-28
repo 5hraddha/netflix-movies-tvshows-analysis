fe1603ba6bb17e37f38dd811d487a03dd7a60745
Shraddha <mailmeatshraddha@gmail.com>
2023-05-28 17:41:23 -0400

    filter Dataframe

--- app.py
+++ app.py
@@ -13,42 +13,41 @@ netflix_data = pd.read_csv('netflix_clean_data.csv')
 # -------------------------------------------------------------------------------------------------
 # Functions
 
-def set_styles(results):
+# Function to set dark theme for Dataframe
+def set_df_styles(results: pd.DataFrame, bg_color: str, font_color: str):
     return (
         results.style
-        .set_properties(**{"background-color": "MistyRose", "color": "black"})
+        .set_properties(**{"background-color": bg_color, "color": font_color})
     )
 
-# Function to select number of rows and columns
-def select_rows_and_columns_from_df(results: pd.DataFrame, key: str):
+# Function to select number of rows and list of columns to filter Dataframe
+def get_attributes_to_filter_df(results: pd.DataFrame, key: str) -> tuple:
     no_of_rows_to_show = st.slider(
         "Select number of table rows to display",
         0,
         len(results) - 1,
         DEFAULT_NUMBER_OF_ROWS,
-        key=key + 'select_rows',
+        key=key + '_select_rows',
     )
 
     columns_to_show = st.multiselect(
         'Select the columns to display',
         ['all', 'show_id', 'type', 'title', 'director', 'cast', 'country', 'date_added', 'year_added', 'release_year', 'rating', 'duration', 'listed_in', 'description'],
-        default='all')
+        default='all',
+        key=key + '_select_columns')
 
-    style = st.checkbox("Style dataframe?", False, key=key + 'table_style')
-    return no_of_rows_to_show, columns_to_show, style
+    is_dark_theme = st.checkbox("Want Dataframe in Dark Theme?", False, key=key + '_df_style')
+    return no_of_rows_to_show, columns_to_show, is_dark_theme
 
+# Function to filter Dataframe based on no of rows and list of columns selected
 @st.cache_data
-def _filter_df_results(results, no_of_rows_to_show, columns_to_show) -> pd.DataFrame:
+def get_filtered_df(results: pd.DataFrame, no_of_rows_to_show: int, columns_to_show: list) -> pd.DataFrame:
     if 'all' in columns_to_show:
-        return results.loc[0:no_of_rows_to_show, :]
-    return results.loc[0:no_of_rows_to_show, columns_to_show]
+        filtered_df = results.loc[0:no_of_rows_to_show, :]
+    else:
+      filtered_df = results.loc[0:no_of_rows_to_show, columns_to_show]
 
-
-def filter_df_results(results, number_of_rows, number_of_columns, style) -> pd.DataFrame:
-    filter_table = _filter_df_results(results, number_of_rows, number_of_columns)
-    if style:
-        filter_table = set_styles(filter_table)
-    return filter_table
+    return filtered_df
 
 # -------------------------------------------------------------------------------------------------
 # Set page title
@@ -82,9 +81,11 @@ columns_dict = [
 
 st.table(pd.DataFrame(columns_dict))
 
-no_of_rows_to_show, columns_to_show, style = select_rows_and_columns_from_df(netflix_data, key="netflix_data")
-filter_table = filter_df_results(netflix_data, no_of_rows_to_show, columns_to_show, style)
-st.dataframe(filter_table)
+no_of_rows_to_show, columns_to_show, is_dark_theme = get_attributes_to_filter_df(netflix_data, key="netflix_data")
+filtered_df = get_filtered_df(netflix_data, no_of_rows_to_show, columns_to_show)
+if is_dark_theme:
+    filtered_df = set_df_styles(filtered_df, 'black', 'white')
+st.dataframe(filtered_df)
 
 # -------------------------------------------------------------------------------------------------
 # Set heading # 2
